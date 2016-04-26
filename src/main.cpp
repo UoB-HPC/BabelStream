6169bdb7b5f887ba3ab8837f153bc00c272991b6
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-04-26 14:40:49 +0100

    Add some global variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 
 #include <iostream>
+#include <vector>
 
 #include "common.h"
 #include "Stream.h"
 #include "CUDAStream.h"
 
 
+const unsigned int ARRAY_SIZE = 52428800;
+
+#define IMPLEMENTATION_STRING "CUDA"
 
 int main(int argc, char *argv[])
 {
   std::cout
     << "GPU-STREAM" << std::endl
     << "Version:" << VERSION_STRING << std::endl
-    << "Implementation: " << std::endl;
+    << "Implementation: " << IMPLEMENTATION_STRING << std::endl;
+
+
+  // Create host vectors
+  std::vector<double> a(ARRAY_SIZE, 1.0);
+  std::vector<double> b(ARRAY_SIZE, 2.0);
+  std::vector<double> c(ARRAY_SIZE, 0.0);
 
   Stream<double> *stream;
   stream = new CUDAStream<double>();
