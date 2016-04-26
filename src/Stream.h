ae679a57752ee1155a7093a7be4740758a9dc6d6
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-04-26 14:50:58 +0100

    Fix indentation in Stream.h

--- src/Stream.h
+++ src/Stream.h
@@ -7,17 +7,18 @@ template <class T>
 class Stream
 {
   public:
-      // Kernels
-      // These must be blocking calls
-      virtual void copy() = 0;
-      virtual void mul() = 0;
-      virtual void add() = 0;
-      virtual void triad() = 0;
+    // Kernels
+    // These must be blocking calls
+    virtual void copy() = 0;
+    virtual void mul() = 0;
+    virtual void add() = 0;
+    virtual void triad() = 0;
 
 
-      // Implementation specific device functions
-      static std::vector<int> getDeviceList();
-      static std::vector<int> getDeviceName();
-      static std::vector<int> getDeviceDriver();
+
+    // Implementation specific device functions
+    static std::vector<int> getDeviceList();
+    static std::vector<int> getDeviceName();
+    static std::vector<int> getDeviceDriver();
 
 };
