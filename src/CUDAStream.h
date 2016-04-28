a745ffc72413ae419afa997d780dda4b47266e3a
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-04-28 12:07:09 +0100

    Add more keywords to CUDA header

--- src/CUDAStream.h
+++ src/CUDAStream.h
@@ -26,8 +26,8 @@ class CUDAStream : public Stream<T>
     virtual void mul() override;
     virtual void triad() override;
 
-    void write_arrays(const std::vector<T>& a, const std::vector<T>& b, const std::vector<T>& c);
-    void read_arrays(std::vector<T>& a, std::vector<T>& b, std::vector<T>& c);
+    virtual void write_arrays(const std::vector<T>& a, const std::vector<T>& b, const std::vector<T>& c) override;
+    virtual void read_arrays(std::vector<T>& a, std::vector<T>& b, std::vector<T>& c) override;
 
 };
 
