530b2adda2e0066e20850c97e7fede9795b65944
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-05-03 12:32:03 +0100

    Add License text to all files

--- CUDAStream.cu
+++ CUDAStream.cu
@@ -1,4 +1,11 @@
 
+// Copyright (c) 2015-16 Tom Deakin, Simon McIntosh-Smith,
+// University of Bristol HPC
+//
+// For full license terms please see the LICENSE file distributed with this
+// source code
+
+
 #include "CUDAStream.h"
 
 void check_error(void)
