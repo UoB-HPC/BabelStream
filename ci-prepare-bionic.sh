7ec28e55f1527d8684981adae946b65e5b2b4900
Tom Lin <tom91136@gmail.com>
2021-03-30 13:02:48 +0100

    CMake: Update CI rocm to 4.1.0

--- ci-prepare-bionic.sh
+++ ci-prepare-bionic.sh
@@ -241,7 +241,7 @@ setup_rocm() {
   sudo apt-get update -qq
   sudo apt-get install -y -qq rocm-dev
   # AMD needs this rocm_path thing exported...
-  export_var ROCM_PATH "/opt/rocm-4.0.0"
+  export_var ROCM_PATH "/opt/rocm-4.1.0"
   export_var HIP_CXX "$ROCM_PATH/bin/hipcc"
   verify_bin_exists "$HIP_CXX"
   "$HIP_CXX" --version
