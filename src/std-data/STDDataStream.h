0b51c76af991abcf1a4f3ffd6aa54d27ee850f01
Tom Deakin <tom.deakin@bristol.ac.uk>
2021-12-22 12:28:28 +0000

    Update src/std-data/STDDataStream.h

--- src/std-data/STDDataStream.h
+++ src/std-data/STDDataStream.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Tom Deakin
+// Copyright (c) 2020 NVIDIA CORPORATION. All rights reserved.
 // University of Bristol HPC
 //
 // For full license terms please see the LICENSE file distributed with this
