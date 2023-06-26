949d2e9319edcc78f15e9364d5af0aacbaa25cec
Tom Deakin <tom.deakin@bristol.ac.uk>
2023-06-26 13:40:22 +0100

    Revert "update name of AOCC 4.0.0 tarball"
    This reverts commit 5c7fd76fa4a35309b314b9c4ce8a3be94a80ea4d.

--- src/ci-prepare-bionic.sh
+++ src/ci-prepare-bionic.sh
@@ -120,7 +120,8 @@ setup_aocc() {
   echo "Preparing AOCC"
 
   local aocc_ver="4.0.0"
-  local tarball="aocc-$aocc_ver.tar"
+  local tarball="aocc-$aocc_ver.tar.xz"
+  # XXX it's actually XZ compressed, so it should be tar.xz
   local AOCC_URL="http://download.amd.com/developer/eula/aocc-compiler-4.0.0.tar"
   # local AOCC_URL="http://localhost:8000/aocc-compiler-2.3.0.tar"
 
