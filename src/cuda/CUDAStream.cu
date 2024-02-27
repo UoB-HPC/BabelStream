607e05022fbf93fb07c2d973ec43567f5fbb88d5
Gonzalo Brito Gadeschi <gonzalob@nvidia.com>
2024-02-27 04:53:18 -0800

    [CUDA] Revert to C++11 for now until CMake is updated

--- src/cuda/CUDAStream.cu
+++ src/cuda/CUDAStream.cu
@@ -13,7 +13,7 @@
   exit(e);
 }
 
-#define CU(EXPR) if (auto __e = (EXPR); __e != cudaSuccess) error(__FILE__, __LINE__, #EXPR, __e);
+#define CU(EXPR) do { auto __e = (EXPR); if (__e != cudaSuccess) error(__FILE__, __LINE__, #EXPR, __e); } while(false)
 
 __host__ __device__ constexpr size_t ceil_div(size_t a, size_t b) { return (a + b - 1)/b; }
 
